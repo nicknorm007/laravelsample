83b35a880d2d0915f28ef6e5e5ec4b1979dfc057
Nick Norman <nicknorm007@gmail.com>
2016-11-06 10:49:01 -0500

    initial commit

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,87 @@
+<?php namespace App\Http\Controllers;
+
+use App\Project;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class ProjectsController extends Controller {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		$projects = Project::all();
+		return view('projects.index', compact('projects'));
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		return view('projects.create');
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  \App\Project $project
+	 * @return Response
+	 */
+	public function show($slug)
+	{
+		$project = Project::where('slug', $slug)->first();
+		return view('projects.show', compact('project'));
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  \App\Project $project
+	 * @return Response
+	 */
+	public function edit($project)
+	{
+		return view('projects.edit', compact('project'));
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \App\Project $project
+	 * @return Response
+	 */
+	public function update($project)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  \App\Project $project
+	 * @return Response
+	 */
+	public function destroy($project)
+	{
+		//
+	}
+
+}
